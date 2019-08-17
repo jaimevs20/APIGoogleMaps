056e6fcd94a1a0c2dc310b8d6d6f79c00ddb2d1a
jaime <jaimevalverde.jv@gmail.com>
2019-08-16 22:16:34 -0300

    Obtendo localização com Geocoder e calculando distâncias.

--- app/src/main/java/com/example/testegmaps/MapsActivity.java
+++ app/src/main/java/com/example/testegmaps/MapsActivity.java
@@ -5,8 +5,13 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.graphics.Color;
 import android.icu.text.CaseMap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -22,8 +27,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
@@ -113,7 +120,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     marker.remove();
                 }
                 //adiciona um novo marcador no local do clique
-                addMarker(new LatLng(local.latitude,local.longitude), "Marker Reposicionado", "Lat: "+local.latitude+" Long: "+local.longitude);
+                addMarker(new LatLng(local.latitude,local.longitude), "Marker posicionado", "Lat: "+local.latitude+" Long: "+local.longitude);
                 lngs.add(local);    //preenche o array com a rota
                 desenhaRota();      //desenha as rotas
             }
@@ -153,7 +160,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void desenhaRota(){
-        PolylineOptions polylineOptions = null;
+        PolylineOptions polylineOptions;
         int tam = lngs.size();
 
         if(polyline == null){
@@ -168,6 +175,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         else{
             polyline.setPoints(lngs);
         }
+    }
 
+    //Obtém a distância
+    public void getDistancia(View view) {
+        double distancia=0;
+
+        for (int i = 0; i<lngs.size();i++){
+            if(i < lngs.size()-1){
+                distancia += calculaDistancia(lngs.get(i),lngs.get(i+1));   //Distância total
+            }
+        }
+        Toast.makeText(MapsActivity.this,"Distância: "+distancia+" metros",Toast.LENGTH_LONG).show();
+    }
+
+    //Calcula a distância entre dois pontos
+    public static double calculaDistancia(LatLng local1, LatLng local2){
+       //Método mais preciso e utilizado pelos desenvolvedores no cálculo das distâncias
+        double lat1 = local1.latitude;
+        double lat2 = local2.latitude;
+        double lon1 = local1.longitude;
+        double lon2 = local2.longitude;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLon = Math.toRadians(lon2-lon1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return 6366000 * c;
+       /*   Location localOrigem = new Location(LocationManager.GPS_PROVIDER),
+                localDestino = new Location(LocationManager.GPS_PROVIDER);
+
+        localOrigem.setLatitude(local1.latitude);
+        localOrigem.setLongitude(local2.longitude);
+
+        localDestino.setLatitude(local2.latitude);
+        localDestino.setLatitude(local2.longitude);
+
+        return localOrigem.distanceTo(localDestino);*/
+       /* if (local1 == null || local2 == null)
+            return Double.parseDouble(null);
+        float[] result = new float[1];
+        Location.distanceBetween(local1.latitude, local2.longitude,
+                local1.latitude, local1.latitude, result);
+        return (double) result[0];*/
+    }
+
+    //Captura a localização do Marker no mapa
+    public void getLocal(View view) throws IOException {
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+
+        //retorna uma lista com informações do endereço
+        List<Address> endereco = geocoder.getFromLocation
+                (lngs.get(lngs.size() - 1).latitude,lngs.get(lngs.size() - 1).longitude,1);
+
+        String rua = endereco.get(0).getThoroughfare(); //retorna o nome da rua
+        String cidade = endereco.get(0).getSubAdminArea(); //retorna o nome da cidade
+        String pais = endereco.get(0).getCountryName(); //retorna o nome do país
+
+        Toast.makeText(MapsActivity.this,"Você está em: "+rua,Toast.LENGTH_LONG).show();
     }
 }
